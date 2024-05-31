bbc8837a089ab35a050cd9fdaff7627d04deeb36
Youss00 <youssra_seghier@yahoo.ca>
2024-05-30 22:10:36 -0400

    amelioration page single

--- single.php
+++ single.php
@@ -7,7 +7,6 @@
             <!-- Séparation des composants du sigle, titre du cours et durée -->
             <?php if (have_posts()) : the_post(); ?>
                 <div class="carte">
-                    <?php the_post_thumbnail('medium') ?>
                     <h2><?php the_title(); ?></h2>
                     <p><?php the_content(); ?></p>
                     <div class="infos-ville">
