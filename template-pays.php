0b771691748fa0db953c23ad2d1b22aec388d651
Seghier <e2139713@cmaisonneuve.qc.ca>
2024-05-31 17:12:27 -0400

    vague et css arranger

--- template-pays.php
+++ template-pays.php
@@ -5,7 +5,7 @@
  *
  */
 get_header(); ?>
-<div id="accueil" class="global">
+<div id="accueil" class="globale">
     <section class="accueil__section">
         <h1>Les plus beaux pays</h1>
         <p>Voyager est une aventure qui enrichit l'âme, ouvre l'esprit et permet de découvrir des cultures, des paysages et des expériences inoubliables. Que vous soyez un explorateur chevronné ou un novice en quête de nouveaux horizons, notre club voyage vous propose une sélection des plus beaux pays à visiter. Préparez-vous à être émerveillé !</p>
@@ -16,14 +16,15 @@ get_header(); ?>
 
 
     </section>
-</div>
 
-<div id="galerie" class="global api">
+</div>
+<?php get_template_part("gabarit/vagues") ?>
+<div id="galerie" class="globale api">
 
     <section class="galerie__section">
         <?php echo do_shortcode('[wtp_country_menu]'); ?>
     </section>
-
+    <?php get_template_part("gabarit/vagues") ?>
 </div>
 <?php
 get_footer();
