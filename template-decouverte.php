fcd4a4f33b31cdc6b626ab52205986b8b7a0ecf6
Youss00 <youssra_seghier@yahoo.ca>
2024-05-31 21:14:45 -0400

    fiche d'inscription fait

--- template-decouverte.php
+++ template-decouverte.php
@@ -13,6 +13,28 @@ get_header(); ?>
         <p>L'adresse de l'evénement<?php the_field('adresse'); ?></p>
         <p>La date et l'heure de l'événement<?php the_field('date_et_heure_de_levenement'); ?></p>
     <?php endif; ?>
+    <div class="inscription">
+        <form action="/submit_registration" method="post">
+            <div class="form-group">
+                <label for="name">Nom:</label>
+                <input type="text" id="name" name="name" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" required>
+            </div>
+            <div class="form-group">
+                <label for="password">Mot de passe:</label>
+                <input type="password" id="password" name="password" required>
+            </div>
+            <div class="form-group">
+                <label for="confirm_password">Confirmez le mot de passe:</label>
+                <input type="password" id="confirm_password" name="confirm_password" required>
+            </div>
+            <button type="submit">S'inscrire</button>
+        </form>
+    </div>
+
 </main><!-- #main -->
 <?php
 get_footer();
